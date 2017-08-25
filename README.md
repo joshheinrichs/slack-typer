3b2e09e19a3ad76ee06bc9a74126bcaa43793f69
Josh Heinrichs <joshiheinrichs@gmail.com>
2017-08-25 02:15:45 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# slack typer [![Travis Ci Status](https://travis-ci.org/joshheinrichs/slack-typer.svg?branch=master)](https://travis-ci.org/joshheinrichs/slack-typer)
+Bring the party to your Slack photo!
+
+## Installing 
+
+Either grab a build on the [releases page](github.com/joshheinrichs/slack-typer/releases) or run:
+
+```bash
+$ go get -u github.com/joshheinrichs/slack-typer
+$ go install github.com/joshheinrichs/slack-typer
+```
+
+## Usage
+
+```bash
+$ slack-typer -slack-token SLACK_TOKEN [-parrot PARROT_GIF] [-h]
+```
+
+Slack tokens can be generated at https://api.slack.com/custom-integrations/legacy-tokens
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	var slackToken, user string
+	flag.StringVar(&slackToken, "slack-token", "",
+		"A Slack API token, generatable at https://api.slack.com/custom-integrations/legacy-tokens")
+	flag.StringVar(&user, "user", "",
+		"Name of the user to type at.")
+	flag.Parse()
+	if slackToken == "" {
+		fmt.Fprintf(os.Stderr, "flag is required: -slack-token\n")
+		flag.Usage()
+		os.Exit(2)
+	}
+	if user == "" {
+		fmt.Fprintf(os.Stderr, "flag is required: -user\n")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	api := slack.New(slackToken)
+	users, err := api.GetUsers()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+		os.Exit(1)
+	}
+
+	userID := ""
+	for _, u := range users {
+		if u.Name == user {
+			userID = u.ID
+		}
+	}
+	if userID == "" {
+		fmt.Fprintf(os.Stderr, "error: invalid user\n")
+		os.Exit(1)
+	}
+
+	_, _, channelID, err := api.OpenIMChannel(userID)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %s\n", err)
+	}
+
+	rtm := api.NewRTM()
+	for {
+		rtm.SendMessage(rtm.NewTypingMessage(channelID))
+		time.Sleep(time.Second)
+	}
+}
